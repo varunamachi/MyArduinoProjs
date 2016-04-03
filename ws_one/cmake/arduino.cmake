597aaf7cfbe6abba23075410d5d5b318a0058a8a
varunamachi <varunamachi@gmail.com>
2016-04-03 22:35:53 +0530

    CMake based build for ws_one

--- ws_one/cmake/arduino.cmake
+++ ws_one/cmake/arduino.cmake
@@ -6,6 +6,7 @@
 # http://www.tmpsantos.com.br/en/2010/12/arduino-uno-ubuntu-cmake/
 # http://forum.arduino.cc/index.php?topic=244741.0
 
+add_definitions(-DARDUINO=160)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER avr-gcc)
@@ -19,22 +20,25 @@ set(TUNNING_FLAGS "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-en
 set(CMAKE_CXX_FLAGS "-mmcu=${ARDUINO_MCU} -DF_CPU=${ARDUINO_FCPU} -Os")
 set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} ${TUNNING_FLAGS} -Wall -Wstrict-prototypes -std=gnu99")
 
-set(ARDUINO_CORE_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/cores/arduino/")
+set(ARDUINO_CORE_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/cores/arduino")
 set(ARDUINO_PINS_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/variants/${ARDUINO_BOARD}")
 set(ARDUINO_PINS_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/variants/${ARDUINO_BOARD}")
+
 set(AVRDUDE_CONFIG "${ARDUINO_ROOT}/hardware/tools/avr/etc/avrdude.conf")
 
 include_directories(${ARDUINO_PINS_DIR})
 include_directories(${ARDUINO_CORE_DIR})
+include_directories(${ARDUINO_ROOT}/hardware/tools/avr/avr/include/ )
 
 set(ARDUINO_SOURCE_FILES
-#        ${ARDUINO_CORE_DIR}/wiring_pulse.S
         ${ARDUINO_CORE_DIR}/wiring_digital.c
         ${ARDUINO_CORE_DIR}/wiring.c
+        ${ARDUINO_CORE_DIR}/hooks.c
+
+#        ${ARDUINO_CORE_DIR}/wiring_pulse.S
 #        ${ARDUINO_CORE_DIR}/WInterrupts.c
 #        ${ARDUINO_CORE_DIR}/wiring_pulse.c
 #        ${ARDUINO_CORE_DIR}/wiring_shift.c
-        ${ARDUINO_CORE_DIR}/hooks.c
 #        ${ARDUINO_CORE_DIR}/wiring_analog.c
 #        ${ARDUINO_CORE_DIR}/WMath.cpp
 #        ${ARDUINO_CORE_DIR}/IPAddress.cpp
@@ -80,3 +84,36 @@ add_custom_target(reset)
 add_custom_command(TARGET reset POST_BUILD
     COMMAND echo 0 > ${PORT}
 )
+
+
+macro( add_arduino_lib LIB_NAME )
+    set(ARDUINO_LIBS_DIR "${ARDUINO_ROOT}/libraries")
+    set(AVR_LIBS_DIR "${ARDUINO_ROOT}/hardware/arduino/avr/libraries" )
+
+    set( LOCAL_LIB_DIR "${CMAKE_SOURCE_DIR}/libs/${LIB_NAME}" )
+    set( ARDUINO_LIB_DIR "${ARDUINO_LIBS_DIR}/${LIB_NAME}/src" )
+    set( AVR_LIB_DIR "${AVR_LIBS_DIR}/${LIB_NAME}/src" )
+
+
+    set( LIB_DIR "" )
+    if( EXISTS ${LOCAL_LIB_DIR} )
+        set( LIB_DIR ${LOCAL_LIB_DIR} )
+        message( STATUS "Added Local Lib: ${LIB_NAME}" )
+    elseif( EXISTS ${ARDUINO_LIB_DIR} )
+        set( LIB_DIR ${ARDUINO_LIB_DIR} )
+        message( STATUS "Added Arduino Lib: ${LIB_NAME}" )
+    elseif( EXISTS ${AVR_LIB_DIR} )
+        set( LIB_DIR ${AVR_LIB_DIR} )
+        message( STATUS "Added AVR Lib: ${LIB_NAME}" )
+    else()
+        message( FATAL_ERROR "Could Not Find Lib: ${LIB_NAME}" )
+    endif()
+    file(GLOB LIB_SRC
+        "${LIB_DIR}/*.h"
+        "${LIB_DIR}/*.cpp"
+        "${LIB_DIR}/*.c"
+        "${LIB_DIR}/*.s"
+    )
+    include_directories( ${LIB_DIR} )
+    set( ARDUINO_SOURCE_FILES ${ARDUINO_SOURCE_FILES} ${LIB_SRC} )
+endmacro()
